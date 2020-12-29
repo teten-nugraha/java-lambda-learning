92ce1b0dd3a9ae372ba4b4eda1d3c7e95c69af5f
Teten Nugraha <teten.nugraha@allianz.co.id>
2020-12-29 17:01:37 +0700

    example 1 - groupping

---
+++ src/main/java/id/ten/lamda/groupping/Example1.java
@@ -0,0 +1,25 @@
+package id.ten.lamda.groupping;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+        List<String> items = Arrays.asList("apple", "apple", "banana",
+                "apple", "orange", "banana", "papaya");
+
+        Map<String, Long> result =
+                items.stream().collect(
+                        Collectors.groupingBy(
+                                Function.identity(), Collectors.counting()
+                        )
+                );
+
+        System.out.println(result);
+    }
+
+}
